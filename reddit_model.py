75b75a4e856b8b141f1f4fdf3f7018d5272fee91
rlin2k1 <rlin2k1@gmail.com>
2019-05-28 20:49:18 -0700

    Loads JSON and CSV Files and Writes DataFrame into Parquet Files

--- reddit_model.py
+++ reddit_model.py
@@ -1,12 +1,22 @@
 from __future__ import print_function
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
+import pandas
 
 
 def main(context):
     """Main function takes a Spark SQL context."""
     # YOUR CODE HERE
     # YOU MAY ADD OTHER FUNCTIONS AS NEEDED
+
+    #---------------------------------------------------------------------------
+    # TASK 1
+    df = context.read.csv('labeled_data.csv')
+    df.write.parquet("labeled_data.parquet")
+    comments = context.read.json("comments-minimal.json.bz2") 
+    df.write.parquet("comments.parquet")
+    submissions = context.read.json("submissions.json.bz2")
+    df.write.parquet("submissions.parquet")
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS143 Project 2B")
