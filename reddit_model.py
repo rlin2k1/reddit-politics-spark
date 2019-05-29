9c847d9f123aa0463d9f7704dcd61e3d8c117020
rlin2k1 <rlin2k1@gmail.com>
2019-05-28 21:16:50 -0700

    Adds Read From Parquet File

--- reddit_model.py
+++ reddit_model.py
@@ -1,7 +1,13 @@
 from __future__ import print_function
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
-import pandas
+import pandas as pd
+from cleantext import sanitize
+
+from pyspark.sql.functions import udf
+# squared_udf = udf(sanitize, ArrayType(StringType()))
+# df = spark.table("test")
+# display(df.select("id", squared_udf("id").alias("id_squared")))
 
 
 def main(context):
@@ -11,12 +17,15 @@ def main(context):
 
     #---------------------------------------------------------------------------
     # TASK 1
-    df = context.read.csv('labeled_data.csv')
-    df.write.parquet("labeled_data.parquet")
-    comments = context.read.json("comments-minimal.json.bz2") 
-    df.write.parquet("comments.parquet")
-    submissions = context.read.json("submissions.json.bz2")
-    df.write.parquet("submissions.parquet")
+    # df = context.read.csv('labeled_data.csv')
+    # df.write.parquet("labeled_data.parquet")
+    # comments = context.read.json("comments-minimal.json.bz2") 
+    # df.write.parquet("comments.parquet")
+    # submissions = context.read.json("submissions.json.bz2")
+    # df.write.parquet("submissions.parquet")
+    labeled_data = context.read.parquet('labeled_data.parquet')
+    comments = context.read.parquet('comments.parquet')
+    submissions = context.read.parquet('submissions.parquet')
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS143 Project 2B")
