27bcf9cecbb0895063941c6371b2c420c5e26f5e
rlin2k1 <rlin2k1@gmail.com>
2019-06-01 16:13:38 -0700

    Adds Count Vectorizor and K Fold Cross Validation for a Logistic Regression Model

--- reddit_model.py
+++ reddit_model.py
@@ -16,6 +16,12 @@ from pyspark.sql.types import ArrayType, StringType # UDF Return Types
 from cleantext import sanitize # Tokenizer
 import pandas as pd # Pandas DataFrame Printing
 
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder 
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+
+from pyspark.ml.feature import CountVectorizer
+
 # ---------------------------------------------------------------------------- #
 # Main Function
 # ---------------------------------------------------------------------------- #
@@ -42,9 +48,51 @@ def main(context):
 
     #---------------------------------------------------------------------------
     # TASK 5
-    sanitized = comments.select(sanitize_udf('body').alias('body_sanitize'))
+    sanitized = comments.select(sanitize_udf('body').alias('raw'))
     df = sanitized.limit(10).toPandas() # Pretty Printing Only
     print(df) # Pretty Printing Only
+
+    #---------------------------------------------------------------------------
+    # TASK 6A
+    cv = CountVectorizer(binary=True, minDF=10.0, inputCol="raw", outputCol="vectors")
+    model = cv.fit(sanitized)
+    model.transform(sanitized).show(truncate=False)
+    countVectorizerPath = "count_vectorizer"
+    cv.save(countVectorizerPath)
+
+    #---------------------------------------------------------------------------
+    # TASK 6B
+
+    #---------------------------------------------------------------------------
+    # TASK 7: MACHINE LEARNING PORTION TO TRAIN MODELS
+    # Initialize two logistic regression models.
+    # Replace labelCol with the column containing the label, and featuresCol with the column containing the features.
+    poslr = LogisticRegression(labelCol="poslabel", featuresCol="features", maxIter=10)
+    neglr = LogisticRegression(labelCol="neglabel", featuresCol="features", maxIter=10)
+    # This is a binary classifier so we need an evaluator that knows how to deal with binary classifiers.
+    posEvaluator = BinaryClassificationEvaluator()
+    negEvaluator = BinaryClassificationEvaluator()
+    # There are a few parameters associated with logistic regression. We do know what they are a priori.
+    # We do a grid search to find the best parameters. We can replace [1.0] with a list of values to try.
+    # We will assume the parameter is 1.0. Grid search takes forever. 
+    posParamGrid = ParamGridBuilder().addGrid(poslr.regParam, [1.0]).build()
+    negParamGrid = ParamGridBuilder().addGrid(neglr.regParam, [1.0]).build()
+    # We initialize a 5 fold cross-validation pipeline.
+    posCrossval = CrossValidator(estimator=poslr, evaluator=posEvaluator, estimatorParamMaps=posParamGrid, numFolds=5)
+    negCrossval = CrossValidator(estimator=neglr, evaluator=negEvaluator, estimatorParamMaps=negParamGrid, numFolds=5)
+    # Although crossvalidation creates its own train/test sets for # tuning, we still need a labeled test set, because it is not # accessible from the crossvalidator (argh!)
+    # Split the data 50/50
+    posTrain, posTest = pos.randomSplit([0.5, 0.5]) 
+    negTrain, negTest = neg.randomSplit([0.5, 0.5]) 
+    # Train the models
+    print("Training positive classifier...") 
+    posModel = posCrossval.fit(posTrain) 
+    print("Training negative classifier...")
+    negModel = negCrossval.fit(negTrain)
+    # Once we train the models, we don't want to do it again. We can save the models and load them again later.
+    posModel.save("project2/pos.model")
+    negModel.save("project2/neg.model")
+    
 
 if __name__ == "__main__":
     conf = SparkConf().setAppName("CS143 Project 2B")
